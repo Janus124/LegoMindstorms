2cd1c357ed9b413fa791f34bad86f634b22c152a
Janus124 <93049908+Janus124@users.noreply.github.com>
2024-05-13 16:54:09 +0200

    Time Recognition in TestInput.java

--- JMALX3/src/TestInput.java
+++ JMALX3/src/TestInput.java
@@ -9,18 +9,59 @@ import lejos.robotics.TouchAdapter;
 
 
 public class TestInput {	
+
+	// create used Sensors
+	// ! Geht das?
+	EV3TouchSensor touchSensor = new EV3TouchSensor(SensorPort.S1);
+	TouchAdapter adapter = new TouchAdapter(touchSensor);
+
+	// List to store inpput from TouchSensor
+	List<String> morseList = new ArrayList<String>();
+
 	public static void main(String[] args) {
-		EV3TouchSensor touchSensor = new EV3TouchSensor(SensorPort.S1);
-		
-		TouchAdapter adapter = new TouchAdapter(touchSensor);
-		while(true) {
-			System.out.println(adapter.isPressed());
-			
+
+		while (true) {
+			long start;
+			long finish;
+
+			if (adapter.isPressed()) {
+				// Sensor is presed, measure Time
+				start = System.currentTimeMillis();
+
+				while (true) {
+					if (!adapter.isPressed()) {
+						// Sensor isn't pressed anymore
+						finish = System.currentTimeMillis();
+						break;
+					}
+					Delay.msDelay(200);
+				}
+				// calculate pressed Time
+				long time = finish - start;
+				System.out.println(time);
+
+				// Save time or stops programm
+				HandleTime(time);
+			}
+
 			Delay.msDelay(1000);
 		}
-		
-		
+
+	}
+
+	public void HandleTime(long time) {
+		if (time < 500) {
+			// short
+			morseList.add(".");
+
+		} else if (time > 500 && time < 2000) {
+			// long
+			morseList.add("-"); // ToDo: - oder _ ???
+
+		} else if (time > 2000) {
+			// stop programm
+			System.exit(0);
+		}
 	}
 
 }
-
