bd626ba2af488031be0feee86eba00a9e4cd17f4
Praktikumsaccount  lego <legoprakt4@codesigni150.informatik.uni-erlangen.de>
2024-05-17 11:06:41 +0200

    updated Imput Method

--- JMALX3/src/TestInput.java
+++ JMALX3/src/TestInput.java
@@ -1,3 +1,6 @@
+import java.util.ArrayList;
+import java.util.List;
+
 import lejos.hardware.Button;
 import lejos.hardware.port.SensorPort;
 import lejos.hardware.sensor.EV3TouchSensor;
@@ -11,20 +14,23 @@ import lejos.robotics.TouchAdapter;
 public class TestInput {	
 
 	// create used Sensors
-	// ! Geht das?
-	EV3TouchSensor touchSensor = new EV3TouchSensor(SensorPort.S1);
-	TouchAdapter adapter = new TouchAdapter(touchSensor);
+	static EV3TouchSensor touchSensor = new EV3TouchSensor(SensorPort.S1);
+	static TouchAdapter adapter = new TouchAdapter(touchSensor);
 
 	// List to store inpput from TouchSensor
-	List<String> morseList = new ArrayList<String>();
+	static List<String> morseList = new ArrayList<String>();
+	
+	static int maxShortPress = 700;
+	static int maxLongPress = 1500;
 
 	public static void main(String[] args) {
-
+		System.out.println("starting");
 		while (true) {
 			long start;
 			long finish;
 
 			if (adapter.isPressed()) {
+				System.out.print("pressed");
 				// Sensor is presed, measure Time
 				start = System.currentTimeMillis();
 
@@ -41,27 +47,31 @@ public class TestInput {
 				System.out.println(time);
 
 				// Save time or stops programm
-				HandleTime(time);
+				
+				//HandleTime(time);
 			}
 
-			Delay.msDelay(1000);
+			Delay.msDelay(200);
 		}
 
 	}
 
-	public void HandleTime(long time) {
+	public static void HandleTime(long time) {
 		if (time < 500) {
 			// short
 			morseList.add(".");
+			System.out.println(".");
 
 		} else if (time > 500 && time < 2000) {
 			// long
 			morseList.add("-"); // ToDo: - oder _ ???
+			System.out.println(".");
 
 		} else if (time > 2000) {
 			// stop programm
 			System.exit(0);
 		}
+
 	}
 
 }
