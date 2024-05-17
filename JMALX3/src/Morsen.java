529f23dca847aaa94773597f7e1a3ea450fad45d
Praktikumsaccount  lego <legoprakt1@codesigni154.informatik.uni-erlangen.de>
2024-05-17 10:57:51 +0200

    morsen file kinda done

--- JMALX3/src/Morsen.java
+++ JMALX3/src/Morsen.java
@@ -1,50 +1,45 @@
-import lejos.hardware.sensor.EV3TouchSensor;
-import lejos.hardware.sensor.SensorMode;
-import lejos.hardware.port.SensorPort;
 import java.util.ArrayDeque;
 import java.util.Arrays;
 import java.util.Deque;
 
+import lejos.hardware.port.SensorPort;
+import lejos.hardware.sensor.EV3TouchSensor;
+
 public class Morsen {
 	public static void main(String[] args) {
-		//Sensor initialisieren | Port S1
-		EV3TouchSensor touch = new EV3TouchSensor(SensorPort.S1);
-		
-		//time
-		long startPressed = 0;
-		long timePressed = 0;
-		long startReleased = 0;
-		long timeReleased = 0;
-		
 		//storage structure
 		//methods: addFirst, addLast, removeFirst, removeLast
 		Deque<String> deque = new ArrayDeque<>();
-		long deqSize = 0;
+		String poisonpill = "POISONPILL";
 		
 		while(true) {
 			//input from sensor
 			//translate to digital signal
 			//@Jonathan
 			String morseChar = "";
+			//Leerzeichen einlesen durch Länge (Zeit)
 			
 			//translate digital signal to letters (mode 0)
 			//@Lea
 			String letter = translate(morseChar);
-			if(letter == "start") {
-				//init Printer
-			} else if(letter == "error") {
+			if(letter == "error") {
 				//error case, remove previous letter
-				if(deqSize >= 1) {
+				if(deque.size() >= 1) {
 					deque.removeLast();
-					deqSize--;
 				}
 			} else if(letter == "end") {
 				//close everything
+				deque.add(poisonpill);
+				break;
 			} else {
-				deque.addLast(letter);
-				deqSize++;
+				deque.add(letter);
+				//Print morsed letter
+				System.out.println(letter);
 			}
 		}
+		
+		//start printer
+		//Printing.startPrinting(deque);
 	}
 	
 	public static String translate(String morseChar) {
