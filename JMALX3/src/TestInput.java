94d3b8cae2f1e2fcdec97a12c348b06af003b94c
Janus124 <93049908+Janus124@users.noreply.github.com>
2024-05-26 18:08:59 +0200

    Added timer for pause in TestInput.java

--- JMALX3/src/TestInput.java
+++ JMALX3/src/TestInput.java
@@ -13,38 +13,58 @@ import lejos.robotics.TouchAdapter;
 
 public class TestInput {	
 
+	//1 Dit zwischen zwei gesendeten Symbolen
+	//3 Dit zwischen Buchstaben in einem Wort
+	//7 Dit schwischen Wörtern
+
 	// create used Sensors
 	static EV3TouchSensor touchSensor = new EV3TouchSensor(SensorPort.S1);
 	static TouchAdapter adapter = new TouchAdapter(touchSensor);
 
 	// List to store inpput from TouchSensor
 	static List<String> morseList = new ArrayList<String>();
-	
-	static int maxShortPress = 700;
-	static int maxLongPress = 1500;
+
+	static long unit = 600;
+	static long maxShortPress = unit;
+	static long maxLongPress = 3 * unit;
+
+	static long minTime = 
 
 	public static void main(String[] args) {
 		System.out.println("starting");
 		while (true) {
-			long start;
-			long finish;
+			long start_press;
+			long finish_press;
 
+			long start_pause;
+			long finish_pause;
+			
+			start_pause = System.currentTimeMillis();
+	
+			
 			if (adapter.isPressed()) {
+				//compute pause
+				finish_pause = System.currentTimeMillis();
+				long time_pause = finish_pause - start_pause;
+				HandlePause(time_pause);
+
+
+				
 				System.out.print("pressed");
 				// Sensor is presed, measure Time
-				start = System.currentTimeMillis();
+				start_press = System.currentTimeMillis();
 
 				while (true) {
 					if (!adapter.isPressed()) {
 						// Sensor isn't pressed anymore
-						finish = System.currentTimeMillis();
+						finish_press = System.currentTimeMillis();
 						break;
 					}
 					Delay.msDelay(200);
 				}
 				// calculate pressed Time
-				long time = finish - start;
-				System.out.println(time);
+				long time_pressed = finish_press - start_press;
+				System.out.println(time_pressed);
 
 				// Save time or stops programm
 				
@@ -57,21 +77,25 @@ public class TestInput {
 	}
 
 	public static void HandleTime(long time) {
-		if (time < 500) {
+		if (time < maxShortPress) {
 			// short
 			morseList.add(".");
 			System.out.println(".");
 
-		} else if (time > 500 && time < 2000) {
+		} else if (time > maxShortPress && time < maxLongPress) {
 			// long
 			morseList.add("-"); // ToDo: - oder _ ???
 			System.out.println(".");
 
-		} else if (time > 2000) {
+		} else if (time > maxLongPress) {
 			// stop programm
 			System.exit(0);
 		}
 
+	}
+
+	public static void HandlePause(long time){
+		//ToDo
 	}
 
 }
