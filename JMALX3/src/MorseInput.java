39bae07328ad73683dea1cd769ccb98fcd125d6e
Janus124 <93049908+Janus124@users.noreply.github.com>
2024-05-27 14:41:44 +0200

    Update MorseInput.java

--- JMALX3/src/MorseInput.java
+++ JMALX3/src/MorseInput.java
@@ -1,186 +1,131 @@
-// If the code is inefficient, consider using StringBuilder
-import java.util.ArrayDeque;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Deque;
-import java.util.List;
-
+package lejostest;
 import lejos.hardware.Button;
+import lejos.hardware.lcd.LCD;
+import lejos.hardware.motor.EV3LargeRegulatedMotor;
+import lejos.hardware.motor.EV3MediumRegulatedMotor;
+import lejos.hardware.port.MotorPort;
 import lejos.hardware.port.SensorPort;
-import lejos.hardware.sensor.EV3TouchSensor;
-import lejos.robotics.SampleProvider;
+import lejos.hardware.sensor.EV3UltrasonicSensor;
+import lejos.robotics.RegulatedMotor;
+import lejos.robotics.chassis.WheeledChassis;
+import lejos.robotics.RangeFinder;
+import lejos.robotics.RangeFinderAdapter;
+import lejos.robotics.Gyroscope;
+import lejos.robotics.GyroscopeAdapter;
 import lejos.utility.Delay;
-import lejos.robotics.TouchAdapter;
+import lejos.robotics.chassis.WheeledChassis;
+import lejos.robotics.chassis.Wheel;
+import lejos.robotics.navigation.MovePilot;
 
-public class MorseInput {
+public class A180DegreeDrift {
 
-    // Create and initialize the touch sensor
-    static EV3TouchSensor touchSensor = new EV3TouchSensor(SensorPort.S1);
-    static TouchAdapter adapter = new TouchAdapter(touchSensor);
-
-    // List to store morsecode
-    static List<String> morseWords = new ArrayList<String>();
-    static String currWord = "";
-    static List<String> normalWords = new ArrayList<String>();
-    static String normalLetters = "";
-
-    // Morse code symbols and pauses
-    // . -> short press (1 unit)
-    // - -> long press (3 units)
-    // s -> short pause (1 unit)
-    // l -> long pause (3 units)
-    // w -> very long pause (7 units)
-
-    // Time definitions in milliseconds
-    static long unit = 600;
-    static long ShortPress = unit;
-    static long LongPress = 3 * unit;
-
-    static long pauseSymbol = unit;
-    static long pauseLetter = 3 * unit;
-    static long pauseWord = 7 * unit;
-
-    static long tolerance = unit / 2;
-
-    public static void main(String[] args) {
-        GetInput();
-    }
-
-	//gets the input from the EV3 Touch Sensor and computes the duration of the time, when the sensor is pushed and paused 
-    public static void GetInput() {
-        System.out.println("Starting");
-        while (true) {
-            long start_press;
-            long finish_press;
-
-            long start_pause;
-            long finish_pause;
-
-            start_pause = System.currentTimeMillis();
-
-            if (adapter.isPressed()) {
-                // Sensor is pressed, measure time
-
-                // Calculate pause duration
-                finish_pause = System.currentTimeMillis();
-                long time_pause = finish_pause - start_pause;
-                if(HandleInput("pause", time_pause) == 0) {
-                	return;
-                }
-
-                System.out.print("Pressed");
-                start_press = System.currentTimeMillis();
-
-                while (true) {
-                    if (!adapter.isPressed()) {
-                        // Sensor is no longer pressed
-                        finish_press = System.currentTimeMillis();
-                        break;
-                    }
-                }
-                // Calculate pressed duration
-                long time_pressed = finish_press - start_press;
-                System.out.println(time_pressed);
-
-
-                if(HandleInput("pressed", time_pressed) == 0) {
-                	return;
-                }
-            }
-        }
-    }
-	//Converts the time with the type into the correct symbols of the morsecode
-	//type = "pause" or "pressed"
-	//time = lenght of the duration pressed or not pressed
-    //-1 error
-    //0 end
-    //1 continue
-    public static int HandleInput(String type, long time) {
-        if (type.equals("pause")) {
-            if (LongInRadius(time, pauseSymbol)) {
-                currWord += "s";
-            } else if (LongInRadius(time, pauseLetter)) {
-                // Letter finished
-                //currWord += "l";
-                String letter = currWord;
-                letter.replace("s", "");
-                normalLetters += translate(letter);
-            } else if (LongInRadius(time, pauseWord)) {
-                // Word finished            
-            	if(".s.s.s-s.s-l" == currWord || (".s.s.s-s.s-" == currWord) || time > 5000) {
-            		//end start translation
-            		//translate(morseWords);
-            		normalWords.add(normalLetters);
-            		normalLetters = "";
-            		return 0;
-            	}
-            	/*
-            	if(currWord.length() == 7 || currWord.length() >= 9 || (currWord.length() == 8 && currWord != "remove")) {
-            		System.out.println("Error: wrong type");
-            	}
-            	*/
-                morseWords.add(currWord);
-                currWord = "";
-            } else {
-                System.out.println("Error: wrong time in HandleInput pause");
-                return -1;
-            }
-        } else if (type.equals("pressed")) {
-            if (LongInRadius(time, ShortPress)) {
-                // Dot
-                currWord += ".";
-            } else if (LongInRadius(time, LongPress)) {
-                // Dash
-                currWord += "-";
-            } else {
-                System.out.println("Error: wrong time in HandleInput pressed");
-                return -1;
-            }
-        } else {
-            System.out.println("Error: wrong type");
-            return -1;
-        }
-        return 1;
-    }
-
-	//checks if the number num is withing a tolerance of expected
-    public static boolean LongInRadius(long num, long expected) {
-        return expected - tolerance < num && expected + tolerance > num;
-    }
-    
-    public static void translate(List<String> mWords) {
-		String[] language = { "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l",
-	            "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", 
-	            "y", "z", "1", "2", "3", "4", "5", "6", "7", "8", "9", "0",
-	            ".", ",", "?", "´", "/", ":", ";", "+", "-", "=", "start", "remove"};
-		String[] morse = { ".-", "-...", "-.-.", "-..", ".", "..-.", "--.", "....", "..", 
-	            ".---", "-.-", ".-..", "--", "-.", "---", ".---.", "--.-", ".-.",
-	            "-", "...", "..-", "...-", ".--", "-..-", "-.--", "--..", ".----",
-	            "..---", "...--", "....-", ".....", "-....", "--...", "---..", "----.",
-	            "-----", ".-.-.-", "--..--", "..--..", ".----.", "-..-.", "---...", "-.-.-.", ".-.-.", "-....-", "-...-", "-.-.-", "........"};
-		String normalLetter = "";
-		while(!mWords.isEmpty()) {
-			//get words
-			String[] letters = mWords.get(0).split("l");
-			String currentWord = "";
-			for(int i = 0; i < letters.length; i++) {
-				String letter = letters[i];
-				letter.replace("s", "");
-				int idx = Arrays.asList(morse).indexOf(letter);
-				if(idx == -1) {
-					System.out.println("Error translation: no idx");
-				} else {
-					String currLetter = language[idx];
-					if(currLetter == "error") {
-						//erase last letter
-						currentWord = currentWord.substring(0, currentWord.length()-1);
-					} else {
-						currentWord += currLetter;
-					}
+	public static void main(String[] args) {
+		
+		System.out.println("Press any button to start");
+		Button.waitForAnyPress();
+		
+		int turnCounter = 0;
+		int maxSpeed = 150;
+		
+		//Create Motor & Sensor for Ultraschall
+		RegulatedMotor motorRight = new EV3LargeRegulatedMotor(MotorPort.B);
+		motorRight.forward();
+		
+		RegulatedMotor motorLeft = new EV3LargeRegulatedMotor(MotorPort.C);
+		motorLeft.forward();
+		
+		EV3UltrasonicSensor sensorForward = new EV3UltrasonicSensor(SensorPort.S1);
+		EV3UltrasonicSensor sensorRight = new EV3UltrasonicSensor(SensorPort.S2);
+		
+		
+		
+		RangeFinder sonarForward = new RangeFinderAdapter(sensorForward);
+		RangeFinder sonarRight = new RangeFinderAdapter(sensorRight);
+		
+		RegulatedMotor[] syncList = {motorLeft};
+		
+		motorRight.synchronizeWith(syncList);
+		
+		//Rotate
+		//Drehen mit default-offset-wert = 10
+		Wheel wheelL = WheeledChassis.modelWheel(motorLeft, 80).offset(10);
+		Wheel wheelR = WheeledChassis.modelWheel(motorRight, 80).offset(-10);
+		WheeledChassis chassis = new WheeledChassis(new Wheel[] {wheelL, wheelR}, WheeledChassis.TYPE_DIFFERENTIAL);
+		MovePilot pilot = new MovePilot(chassis);
+		
+		//best value 1400
+		while(Button.getButtons() == 0) {
+		
+			float distanceValueF = sonarForward.getRange();
+			float distanceValueR = sonarRight.getRange();
+			
+			if (turnCounter == 2) {
+				//long wall with bin
+				 if(10 <= distanceValueR && distanceValueR <= 15) {
+					MoveForward( motorRight, motorLeft, maxSpeed);
+				} else if(distanceValueR >= 15) {
+					//turn right 
+					motorRight.startSynchronization();
+					motorRight.setSpeed(maxSpeed -30);
+					motorLeft.setSpeed(maxSpeed);
+					motorRight.endSynchronization();
+				} else if(distanceValueR <= 10 || distanceValueF <= 15) {
+					//turn left & handle bin
+					motorRight.startSynchronization();
+					motorRight.setSpeed(maxSpeed);
+					motorLeft.setSpeed(maxSpeed -30);
+					motorRight.endSynchronization();
 				}
+				
+			}else if(10 <= distanceValueR && distanceValueR <= 40) {
+				MoveForward( motorRight, motorLeft, maxSpeed);
+			} else if(distanceValueR >= 15 && distanceValueR <= 25) {
+				//turn right 
+				motorRight.startSynchronization();
+				motorRight.setSpeed(maxSpeed -30);
+				motorLeft.setSpeed(maxSpeed);
+				motorRight.endSynchronization();
+			} else if(distanceValueR <= 10) {
+				//turn left
+				motorRight.startSynchronization();
+				motorRight.setSpeed(maxSpeed);
+				motorLeft.setSpeed(maxSpeed -30);
+				motorRight.endSynchronization();
+			} else if (distanceValueR > 25) {
+				//180° kurve
+				//turn right 
+				MoveForward( motorRight, motorLeft, maxSpeed / 2);				
+				Delay.msDelay(100);
+				
+				MoveForward( motorRight, motorLeft, 0);				
+				Delay.msDelay(100);
+				
+				pilot.rotate(90);
+				
+				MoveForward( motorRight, motorLeft, maxSpeed / 2);	
+				
+				Delay.msDelay(1000);
+				
+				pilot.rotate(100);
+				
+				turnCounter++;
+				
 			}
-			//in neue Liste einfügen
-			normalWords.add(currentWord);
 		}
+		
+		motorRight.startSynchronization();
+		motorRight.close();
+		motorLeft.close();
+		motorRight.endSynchronization();
+	}
+	
+	
+	
+	public static void MoveForward(RegulatedMotor motorRight, RegulatedMotor motorLeft, int speed) {
+		motorRight.startSynchronization();
+		motorRight.setSpeed(speed);
+		motorLeft.setSpeed(speed);
+		motorRight.endSynchronization();
+
 	}
 }
